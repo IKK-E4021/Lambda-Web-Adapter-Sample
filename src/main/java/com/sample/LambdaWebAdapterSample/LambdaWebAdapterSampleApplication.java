a4667858bee1b38b1cabc403d243bd2a872fb3a1
i.kimura <kimkaku1204@gmail.com>
2023-11-21 17:42:35 +0900

    Webアプリケーション土台の実装

---
+++ src/main/java/com/sample/LambdaWebAdapterSample/LambdaWebAdapterSampleApplication.java
@@ -0,0 +1,13 @@
+package com.sample.LambdaWebAdapterSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LambdaWebAdapterSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LambdaWebAdapterSampleApplication.class, args);
+	}
+
+}
