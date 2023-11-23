99e14756da33a5e255c7cc75b5f161505c80970f
i.kimura <kimkaku1204@gmail.com>
2023-11-23 23:41:30 +0900

    ECRへイメージをpush

---
+++ Dockerfile
@@ -0,0 +1,30 @@
+# ステージ1: Spring Boot APIサーバービルド (Gradleを使用)
+FROM gradle:8.4.0-jdk17 AS backend-build
+WORKDIR /app
+COPY backend/build.gradle.kts /app
+COPY backend/src /app/src
+RUN gradle build -x test
+
+# ステージ2: Reactフロントエンドビルド
+FROM node:20.9.0 AS frontend-build
+WORKDIR /app
+COPY frontend/package.json frontend/package-lock.json* /app/
+RUN npm ci
+COPY frontend/ /app/
+RUN npm run build
+
+# 最終ステージ: イメージ組み立て
+FROM amazoncorretto:17.0.9
+# ソースコードそのままでLambdaを動かすためのコマンド
+#COPY --from=public.ecr.aws/awsguru/aws-lambda-adapter:0.7.1 /lambda-adapter /opt/extensions/lambda-adapter
+# APIサーバーアプリケーションのコピー
+COPY --from=backend-build /app/build/libs/*.jar /app/app.jar
+# フロントエンドビルドのコピー
+COPY --from=frontend-build /app/build /app/public
+# データベースファイル（SQLiteなど）をコピーする場合はここに追加
+
+# ポート設定
+EXPOSE 8080
+
+# 起動コマンド
+CMD ["java", "-jar", "/app/app.jar"]
--- backend/src/main/java/com/sample/LambdaWebAdapterSample/SampleController.java
+++ backend/src/main/java/com/sample/LambdaWebAdapterSample/SampleController.java
@@ -4,25 +4,16 @@ import lombok.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
-
 @RestController
 @RequestMapping("")
 @CrossOrigin(origins = "http://localhost:3000")
 public class SampleController {
 
-    private final SampleRepository sampleRepository;
-
-    public SampleController(SampleRepository sampleRepository) {
-        this.sampleRepository = sampleRepository;
-    }
-
     @GetMapping("/{id}")
-    public ResponseEntity<SampleResponse> sample(@PathVariable int id) {
-        Map<String, Object> map = sampleRepository.findById(id);
+    public ResponseEntity<SampleResponse> sample() {
         var sampleResponse = SampleResponse
                 .builder()
-                .name(map.get("name"))
+                .name("Jack")
                 .build();
         return ResponseEntity.ok().body(sampleResponse);
     }
@@ -30,7 +21,7 @@ public class SampleController {
     @Data
     @Builder
     public static class SampleResponse {
-        private Object name;
+        private String name;
     }
 
 }
