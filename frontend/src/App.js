f6cd45779efaa6a27e7d090de35f60093849603c
i.kimura <kimkaku1204@gmail.com>
2023-11-26 16:50:24 +0900

    不要な画像ファイル読み込み箇所を削除

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import axios from "axios";
 function App() {
 
   const instance = axios.create({
-    baseURL: process.env.REACT_APP_API_BASE_URL ? process.env.REACT_APP_API_BASE_URL : "http://localhost:8081",
+    baseURL: process.env.REACT_APP_API_BASE_URL,
     headers: {
       'Content-Type': 'application/json'
     },
